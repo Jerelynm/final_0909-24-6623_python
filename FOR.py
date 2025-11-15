c84f7d73576a745c613c1ee9b139fceb728b9ba1
Jerelynm <jmarinm4@miumg.edu.gt>
2025-11-15 08:51:38 -0600

    Se modifica el Ejercicio 1

--- FOR.py
+++ FOR.py
@@ -1,5 +1,3 @@
-#REALIZACIÓN DE LA TABLA DEL 5 CON NÚMEROS PARES UTILIZANDO FOR
-
 n = int(input("Número para la tabla: "))
-for i in range(2, n*1, 2):
+for i in range(2, 11, 2):  # Del 2 al 10, solo pares
     print(f"{n} x {i} = {n * i}")
